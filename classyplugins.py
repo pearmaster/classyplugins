0f4fd858097ae7fe3847bd4c5074060cab79bf64
Jacob Brunson <classyplugins@jacobbrunson.com>
2013-11-18 14:38:54 -0700

    fixed module name

--- classyplugins.py
+++ classyplugins.py
@@ -19,12 +19,14 @@ class ClassyPlugins(object):
         module object.  It does not currently load the module into globals
         """
         dirs = [os.path.dirname(filename)]
-        name = os.path.basename(filename).strip(".py")
+        name = os.path.basename(filename)
+        if name.endswith(".py"):
+            name = name[:-3]
         # File the python module at the directory/name we specified.
         _file, path, des = imp.find_module(name, dirs)
         mod = None
         try:
-            mod = imp.load_module("foo", _file, path, des)
+            mod = imp.load_module(name, _file, path, des)
         finally:
             # Always close the open file handle from imp.find_module()
             if _file:
